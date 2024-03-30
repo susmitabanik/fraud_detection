3942b213a6a98e8dccbd2766eab836497a6fef0f
susmitabanik <susmita.banik56@gmail.com>
2024-03-30 11:06:41 +0100

    Added ML model 3 code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all ML model related code here")
+
+def model1():
+    print("Adding code for ML model1")
+
+def model2():
+    print("Adding code for ML model2")
+
+def model3():
+    print("Adding code for ML model3")
